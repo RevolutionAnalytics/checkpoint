9cc594e1c8d4da68b107aac5dda70e0d01b34ed3
Scott Chamberlain <myrmecocystus@gmail.com>
2014-06-03 11:25:58 -0700

    started fxn for determining repo dependencies, #3

---
+++ R/repodeps.R
@@ -0,0 +1,29 @@
+#' Get repository dependencies
+#'
+#' @import miniCRAN packrat
+#' @export
+#' @param repo A path to a RRT repository; defaults to current working directory.
+#' @return A named list of packages, named by the package that requires said dependencies
+#' @examples
+#' repodeps(repo="~/myrepo")
+#' repodeps(repo="~/myrepo", simplify=TRUE)
+#' setwd("~/myrepo")
+#' repodeps()
+
+repodeps <- function(repo=getwd(), simplify=FALSE)
+{
+  # Get packages used in the repo using appDependencies from packrat
+  pkgs_used <- packrat:::appDependencies(repo)
+  pkgs_used <- pkgs_used[!grepl("packrat", pkgs_used)]
+  
+  # Get package dependencies using miniCRAN
+  pkg_deps <- lapply(pkgs_used, pkgDep)
+  names(pkg_deps) <- pkgs_used
+  
+  if(simplify){
+    allpkgs <- unname(do.call(c, pkg_deps))
+    pkg_deps <- unique(allpkgs)
+  }
+  
+  return(pkg_deps)
+}
