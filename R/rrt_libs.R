1809e69337d32ced5ecd1ef099e8a7332f8c8bc4
Scott Chamberlain <myrmecocystus@gmail.com>
2014-06-18 13:34:57 -0700

    fixed problem in rrt_repos_list where failed on missing repo, closes #29

--- R/rrt_libs.R
+++ R/rrt_libs.R
@@ -1,18 +1,11 @@
-#' Write rrt libraries to user's .Rprofile file
+#' @title Write rrt libraries to user's .Rprofile file
 #' 
 #' @export
-#' @keywords internal
 #' @param repo Respository path
 #' @param repoid Repository id
 #' 
-#' @details This function is used internally to write a .rrt file in your home directory, and 
-#' within that file writes a list of all RRT repositories. 
-#' 
-#' If for some reason your .rrt file gets deleted, you can also use this function to rewrite that 
-#' file and its contents. Ideally you could make a vector or list of all your RRT repositories 
-#' and pass those in an apply like functon to this function, like 
-#' \code{lapply(repos, rrt_repos_write)}, where \code{repos} is a list of path names to RRT repos.
-#' 
+#' @rdname rrt_repos_list
+#'
 #' @examples \dontrun{
 #' rrt_repos_write(repo="~/testrepo/")
 #' }
@@ -42,10 +35,19 @@ rrt_repos_write <- function(repo, repoid=NULL){
   }
 }
 
-#' Read rrt libraries from user's .Rprofile file
+#' @title Read rrt libraries from user's .Rprofile file
 #' 
 #' @export
 #' @param repoid Respository id, default is NULL, so gets all repos
+#' 
+#' @details This function is used internally to write a .rrt file in your home directory, and 
+#' within that file writes a list of all RRT repositories. 
+#' 
+#' If for some reason your .rrt file gets deleted, you can also use this function to rewrite that 
+#' file and its contents. Ideally you could make a vector or list of all your RRT repositories 
+#' and pass those in an apply like functon to this function, like 
+#' \code{lapply(repos, rrt_repos_write)}, where \code{repos} is a list of path names to RRT repos.
+#' 
 #' @examples \dontrun{
 #' (repos <- rrt_repos_list())
 #' names(repos)
@@ -68,14 +70,19 @@ rrt_repos_list <- function(repoid=NULL){
     
     # get other info from each repo's local manifest file
     out <- lapply(out, function(x){ 
-      tmp <- readLines(x$repo)
-      tmp2 <- do.call(c, lapply(tmp, function(y){ yy <- strsplit(y, ":")[[1]]; zz <- yy[2]; zz <- gsub('\\s+', '', zz); names(zz) <- yy[1]; as.list(zz) }))
-      tmp2$repo_root <- sub("/rrt/rrt_manifest.txt", "", x[['repo']])
-      c(x['repo'], tmp2)
+      if(file.exists(x$repo)){
+        tmp <- readLines(x$repo)
+        tmp2 <- do.call(c, lapply(tmp, function(y){ yy <- strsplit(y, ":")[[1]]; zz <- yy[2]; zz <- gsub('\\s+', '', zz); names(zz) <- yy[1]; as.list(zz) }))
+        tmp2$repo_root <- sub("/rrt/rrt_manifest.txt", "", x[['repo']])
+        c(x['repo'], tmp2, missing=FALSE)
+      } else {
+        addfields <- c(RepositoryName = NA, Authors = NA, License = NA, Description = NA, Remote = NA, InstalledWith = NA, InstalledFrom = NA, RRT_version = NA, R_version = NA, DateCreated = NA, PkgsInstalledAt = NA, x['RepoID'], Packages = NA, SystemRequirements = NA, repo_root = NA)
+        c(x['repo'], addfields, missing=TRUE)
+      }
     })
     
     class(out) <- 'rrtrepos'
-    if(is.null(repoid)) out  else out[[repoid]]
+    if(is.null(repoid)) out else out[[repoid]]
   } else { stop("You have no rrt repos or your .rrt file does not exist.\nIf the latter, run rrt_repos_write() with paths for each RRT repository.") } 
 }
 
@@ -85,11 +92,51 @@ rrt_repos_list <- function(repoid=NULL){
 #' @param x Input
 #' @param ... not used
 #' @rdname rrt_repos_list
+# print.rrtrepos <- function(x, ...){
+#   repos <- sapply(x, "[[", "repo")
+#   repoids <- paste(sapply(x, "[[", "RepoID"), collapse = ", ")
+#   cat(sprintf("No. repos: %s", length(repos)), "\n")
+#   cat(sprintf("First 10 repo ids : %s", repoids), "\n\n")
+#   cat("To get details for a single RRT repository, do rrt_repos_list('path/to/repo')", "\n")
+# }
+
 print.rrtrepos <- function(x, ...){
   repos <- sapply(x, "[[", "repo")
   repoids <- paste(sapply(x, "[[", "RepoID"), collapse = ", ")
-  cat(sprintf("No. repos: %s", length(repos)), "\n")
-  cat(sprintf("First 10 repo ids : %s", repoids), "\n")
+  paths <- lapply(x, function(b) c(b$repo_root, b$RepoID))
+  misspaths <- paths[vapply(paths, function(n) is.na(n[[1]]), TRUE)]
+  notmisspaths <- paths[!vapply(paths, function(n) is.na(n[[1]]), TRUE)]
+  
+  df <- data.frame(do.call(rbind, unname(notmisspaths)), stringsAsFactors = FALSE)
+  names(df) <- c('path','repoid')
+  
+  if(!length(misspaths) == 0){
+    idmiss <- names(misspaths)
+    idmiss_list <- x[names(x) %in% idmiss]
+    idmisstodf <- list()
+    for(i in seq_along(idmiss_list)){
+      tmp <- idmiss_list[[i]]$repo
+      tmp <- sub("/rrt/rrt_manifest.txt", "", tmp)
+      idmisstodf[[i]] <- c(tmp, names(idmiss_list[i]))
+    }
+    dfmiss <- data.frame(do.call(rbind, idmisstodf), stringsAsFactors = FALSE)
+    names(dfmiss) <- c('path','repoid')
+  } else { dfmiss <- "all good :)" }
+  
+  cat(wrap(sprintf("Number of RRT repos: %s", length(repos)), width = 80), "\n\n")
+  cat(wrap("RRT repos : \n\n", width = 80), "\n\n")
+  print(df)
+  cat("\n\n")
+  cat(wrap("RRT repos in .rrt file, but repo not found: \n\n", width = 80), "\n\n")
+  print(dfmiss)
+  cat("\n\n")
+  cat("Heads up -> To get details for a single RRT repo: rrt_repos_list('path/to/repo')")
+}
+
+wrap <- function (..., indent = 0, width=getOption("width")) {
+  x <- paste0(..., collapse = "")
+  wrapped <- strwrap(x, indent = indent, exdent = indent + 2, width = width)
+  paste0(wrapped, collapse = "\n")
 }
 
 rrt_repos_check <- function(){
