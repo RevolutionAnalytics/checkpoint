cdacb132172e7c878e0653d6f3142b71661848e3
Scott Chamberlain <myrmecocystus@gmail.com>
2014-07-15 15:51:09 -0700

    fixed spelling error of install.packages

--- R/rrt_install2.R
+++ R/rrt_install2.R
@@ -13,10 +13,10 @@ rrt_install2 <- function(repo=getwd(), repoid, lib, suggests=FALSE, verbose=TRUE
 {
 #   pkgslist <- paste0(lib, "/src/contrib/PACKAGES")
   pkgslist <- list.files(file.path(lib, "src/contrib"))
-  
+
   mssg(verbose, "Looking for packages used in your repository...")
   x <- repodeps(repo, simplify = TRUE, base=FALSE, suggests=suggests)
-  
+
   if(length(pkgslist) == 0) {
     mssg(verbose, "Getting new packages...")
     pkgs2install <- getPkgs(x, repo, lib, verbose=verbose)
@@ -25,10 +25,10 @@ rrt_install2 <- function(repo=getwd(), repoid, lib, suggests=FALSE, verbose=TRUE
     installedpkgs <- installedpkgs[!installedpkgs %in% "src"]
     pkgs2install <- sort(x)[!sort(x) %in% sort(installedpkgs)]
   }
-  
+
   basepkgs <- rownames(installed.packages(priority="base"))
   pkgs2install <- pkgs2install[!pkgs2install %in% basepkgs]
-  
+
   if(length(pkgs2install)==0){
     mssg(verbose, "No packages found to install")
   } else {
@@ -43,13 +43,13 @@ rrt_install2 <- function(repo=getwd(), repoid, lib, suggests=FALSE, verbose=TRUE
     } else {
       # install github pkgs separately
       gh_install <- pkgswithpath[grep("\\.zip", pkgswithpath)]
-      gh_install_names <- vapply(gh_install, 
+      gh_install_names <- vapply(gh_install,
                                  function(x) sub("\\.zip", "", strsplit(x, '/')[[1]][length(strsplit(x, '/')[[1]])]),
                                  character(1), USE.NAMES = FALSE)
       for(i in seq_along(gh_install_names)){
         install_other(pkg=gh_install_names[i], lib=lib)
       }
-      
+
       # regular install from tar.gz
       pkgswithpath <- pkgswithpath[!grepl("\\.zip", pkgswithpath)]
       pkgswithpath <- unlist(pkgswithpath)
@@ -58,8 +58,8 @@ rrt_install2 <- function(repo=getwd(), repoid, lib, suggests=FALSE, verbose=TRUE
       oldwd <- getwd()
       on.exit(setwd(oldwd))
       setwd(dirname(pkgswithpath[1]))
-      install.packagess(basename(pkgswithpath), lib = lib, repos=NULL, type = "source", dependencies=FALSE)
-      
+      install.packages(basename(pkgswithpath), lib = lib, repos=NULL, type = "source", dependencies=FALSE)
+
       notinst <- pkgs2install[!vapply(file.path(lib, pkgs2install), file.exists, logical(1))]
       if(!length(notinst) == 0) install.packages(notinst, lib = lib, destdir = file.path(lib, "src/contrib"))
     }
