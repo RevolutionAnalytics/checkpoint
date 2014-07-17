ab5efbc286ec66ebad76a80408430255926b0bb1
Scott Chamberlain <myrmecocystus@gmail.com>
2014-07-17 14:52:01 -0700

    added function to simplify learning what pkgs are installed in your rrt repo closes #105

---
+++ R/rrt_installed_pkgs.R
@@ -0,0 +1,30 @@
+#' Check what packages are installed.
+#' 
+#' @export
+#' 
+#' @param repo A repository path. This is the path to the root of your RRT repository. By default,
+#' we use the current working directory via \code{getwd()}.
+#' @param simplify (logical) Whether to simplify to just package name and version or print entire
+#' matrix of package information.
+#' @examples \dontrun{
+#' rrt_installed_pkgs()
+#' rrt_installed_pkgs(simplify=FALSE)
+#' }
+
+rrt_installed_pkgs <- function(repo=getwd(), simplify=TRUE)
+{
+  if(is_rrt(repo, verbose = FALSE)){
+    lib <- rrt_libpath(repo)
+    tmp <- installed.packages(lib.loc = lib)
+    if(simplify){
+      if(!NROW(tmp) == 0){
+        df <- tmp[,c("Package",'Version')]
+        row.names(df) <- NULL
+        data.frame(df, stringsAsFactors = FALSE)
+      }
+    }
+    else { tmp }
+  } else {
+    message("This is not an RRT repository :(")
+  }
+}
