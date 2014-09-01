6e2e139f806ce7275d02ec439997b55bdbc17ca8
Andrie <andrie@revolutionanalytics.com>
2014-09-01 16:17:31 +0100

    Moved examples to inst/examples/example_install_mran.R

--- inst/examples/example_install_mran.R
+++ inst/examples/example_install_mran.R
@@ -1,5 +1,18 @@
 \dontrun{
-install_mran(pkg="plyr", date="2014-06-23", lib="~/mran_snaps/")
-install_mran(pkg="calmate", date="2014-06-23", lib="~/mran_snaps/")
+  libPath <- tempdir()
+  instPath <- file.path(libPath, "src/contrib")
+  if(file.exists(instPath)) unlink(instPath, recursive = TRUE)
+  if(!file.exists(instPath)) dir.create(instPath)
+  dir(instPath)
+  
+  install_mran(pkg="plyr", date="2014-08-23", lib=instPath)
+  
+  dir(instPath)
+  unlink(instPath, recursive = TRUE)
 }
 
+
+
+getSnapshotId(date='2014-08-04')
+download_pkgs_mran(date='2014-08-04', pkgs=c("plyr","ggplot2"), lib=libPath)
+dir(instPath)
