fc39fdd633a7ac582c2812406a2c8f34f585a080
Andrie <andrie@revolutionanalytics.com>
2014-09-01 13:42:14 +0100

    Renamed mran_fxns.R to mran.R and merged code from mran_set.R

--- R/mran.R
+++ R/mran.R
@@ -136,3 +136,42 @@ snapshot_from_date <- function(x){
     suppressMessages(mran_snaps(x))
   }
 }
+
+
+#' Set the MRAN snapshot to get packages from.
+#'
+#' @import httr RJSONIO
+#' @export
+#'
+#' @param snapshot A MRAN snapshot. Defaults to most recent snapshot
+#' @param repo Repository path.
+#'
+#' @family mran
+#' 
+#' @examples \dontrun{
+#' mran_set()
+#' mran_set(snapshot="2014-07-01_2000")
+#' }
+
+mran_set <- function(snapshot=NULL, repo=getwd())
+{
+  if(is.null(snapshot)){
+    gg <- suppressMessages(mran_snaps())
+    snapshot <- gg[length(gg)] # get the latest snapshot (latest date that is)
+  }
+  
+  url <- sprintf("%s/%s/", file.path(mran_server_url(), 'snapshots'), snapshot)
+  res <- GET(url)
+  if(res$status_code >= 300)
+    stop(sprintf("%s - snapshot not found, you don't have an internet connection, or other error...", res$status_code))
+  
+  # check for folder, and rrt folder inside
+  check4repo(repo, TRUE)
+  lib <- rrt_libpath(repo)
+  check4rrt(repo, lib, TRUE)
+  
+  # write MRAN snapshot id
+  message("Writing new snapshotID...")
+  writeManifest(repo, lib, packs = NULL, repoid = digest(normalizePath(repo)), snapshot = snapshot)
+  message("...Done")
+}
