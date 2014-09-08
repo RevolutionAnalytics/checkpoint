3aad8596a125fc86c12accc192ab270d7ccd095e
Andrie <andrie@revolutionanalytics.com>
2014-09-08 16:51:45 +0100

    Updated unit tests for rrt_sweep

--- tests/testthat/test-9-rrt-sweep.R
+++ tests/testthat/test-9-rrt-sweep.R
@@ -9,34 +9,53 @@ options(repos=c(CRAN="http://cran.revolutionanalytics.com/"))
 
 
 test_that("sweep works as expected", {
-  rrt_init(repo=rrt_path, verbose = FALSE, autosnap = TRUE, quiet=TRUE)
+  checkpoint(repo=rrt_path, snapshotdate="2014-08-01", verbose = FALSE)
   cat("library(stringr)", file=file.path(rrt_path, "code.R"))
-  rrt_refresh(repo=rrt_path, autosnap = TRUE, verbose = FALSE, quiet=TRUE)
+  checkpoint(repo=rrt_path)
 
   expect_true(is_rrt(rrt_path, FALSE))
   expect_false(is_rrt("~/", FALSE))
-  expect_true(all(c("code.R", "rrt") %in% list.files(rrt_path)))
+  expect_equal(
+    list.files(rrt_path),
+    c("code.R", "manifest.yml", "rrt")
+  )
+  
+  installed <- list.files(rrtPath(rrt_path, "lib"))
+  installed_pre <- installed[!installed %in% "src"]
+  src_pre <- list.files(rrtPath(rrt_path, "src"))
+  expect_equal(installed_pre, c("stringr"))
+
+  
 })
 
+
 test_that("sweep returns messages", {
-  expect_message(rrt_sweep(repo=rrt_path), "Checking to make sure repository exists")
-  expect_message(rrt_sweep(repo=rrt_path), "Package sources removed")
-  expect_message(rrt_sweep(repo=rrt_path), "Checking to make sure rrt directory exists inside your repository")
+  expect_message(
+    rrt_sweep(repo=rrt_path), 
+    "Checking to make sure repository exists"
+  )
+  expect_message(
+    rrt_sweep(repo=rrt_path), 
+    "Package sources removed"
+  )
+  expect_message(
+    rrt_sweep(repo=rrt_path),
+    "Checking to make sure rrt directory exists inside your repository"
+    )
+  expect_equal(
+    list.files(rrt_path),
+    c("code.R", "manifest.yml", "rrt")
+  )
+  
 })
 
 
 test_that("sweep actually removes installed packages and sources", {
   options(verbose=FALSE)
-  rrt_install(rrt_path, quiet=TRUE)
-  installed <- list.files(rrtPath(rrt_path, "lib"))
-  installed_pre <- installed[!installed %in% "src"]
-  src_pre <- list.files(rrtPath(rrt_path, "src"))
   
-  expect_message(rrt_sweep(rrt_path), "stringr")
   installed <- list.files(rrtPath(rrt_path, "lib"))
   installed_post <- installed[!installed %in% "src"]
   src_post <- list.files(rrtPath(rrt_path, "src"))
-  expect_equal(installed_pre, c("stringr"))
   expect_equal(installed_post, character(0))
   expect_equal(src_post, character(0))
   
