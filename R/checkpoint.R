ad0037ec9414bb6c35f5034e235ba94bda0d062f
Antonio Piccolboni <antonio@piccolboni.info>
2014-10-08 14:47:34 -0700

    @andrie, in theory calling installed.pacakges(priority = "base") seemed a good idea, in practice it returns empty after resetting libPaths the first time. @revodavid "recommended" packages are on MRAN, so not excluded for now. I put in a hard coded list of "base" packages, if we support only R 3.1.1 it is an adequate, if not pretty, solution.

--- R/checkpoint.R
+++ R/checkpoint.R
@@ -49,7 +49,10 @@ checkpoint <- function(snapshotDate, project = getwd(), verbose=TRUE) {
 
   # Scan for packages used
   mssg(verbose, "Scanning for packages used in this project")
-  exclude.packages = c("checkpoint", "stats", "graphics", "grDevices", "utils", "datasets", "methods", "base")
+  exclude.packages = c("checkpoint", # this very package
+                       c("base", "compiler", "datasets", "graphics", "grDevices", "grid",
+                         "methods", "parallel", "splines", "stats", "stats4", "tcltk",
+                         "tools", "utils"))  # all base priority packages, not on CRAN or MRAN
   packages.to.install = setdiff(projectScanPackages(project), exclude.packages)
 
   # install missing packages
