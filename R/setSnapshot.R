6664d06199a968137e0b2ee7a26ea0b27e9e1f8d
Andrie de Vries <apdevries@gmail.com>
2015-04-27 15:43:08 +0100

    Add function setSnapshot() to easily point to alternative MRAN mirror. Closes #148

---
+++ R/setSnapshot.R
@@ -0,0 +1,18 @@
+#' Set default CRAN repository to MRAN snapshot date.
+#' 
+#' @inheritParams checkpoint
+#' 
+#' @export
+#' @example /inst/examples/example_setSnapshot.R
+#' 
+setSnapshot <- function(snapshotDate){
+  if (missing(snapshotDate) || is.null(snapshotDate)) return(getOption("repos"))
+  repoDate <- paste0("http://mran.revolutionanalytics.com/snapshot/", snapshotDate)
+  response <- tryCatch(
+    suppressWarnings(readLines(repoDate)),
+    error = function(e)e
+  )
+  if(inherits(response, "error")) stop(paste0("Invalid snapshot date."))
+  options(repos = c(CRAN = repoDate))
+  message(paste("Using CRAN mirror at", repoDate))
+}
