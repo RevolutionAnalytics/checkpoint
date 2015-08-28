47b453b684e8989382de5d5c5380b2ef05459088
Andrie de Vries <apdevries@gmail.com>
2015-08-28 10:33:02 +0100

    Wrap url() in tryCatch() to catch additional cases of https not supported #190

--- R/mranUrl.R
+++ R/mranUrl.R
@@ -128,8 +128,12 @@ url <- function(url){
 }
 
 httpsSupported <- function(mran = "https://mran.revolutionanalytics.com/snapshot/"){
-  con <- url(mran)
   on.exit(close(con))
+  con <- suppressWarnings({
+    tryCatch(url(mran), 
+             error = function(e)e)
+  }) 
+  if(inherits(con, "error")) return(FALSE)
   x <- suppressWarnings(
     tryCatch(readLines(con, warn = FALSE), 
              error = function(e)e)
