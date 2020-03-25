3b8ec360f3fb27627d8824170dec28abfa852e37
Hong Ooi <hongooi@microsoft.com>
2020-03-25 15:44:56 +1100

    add checkpoint master func

--- tests/testthat/test02a_checkpoint1.R
+++ tests/testthat/test02a_checkpoint1.R
@@ -1,9 +1,10 @@
-context("Checkpointing")
+context("Checkpointing 1")
 
 skip_on_cran()
 
 mran <- getOption("checkpoint.mranUrl", "https://mran.microsoft.com")
 snapshot <- "2020-01-01"
+snapshot2 <- "2019-01-01"
 checkpoint_loc <- tempfile()
 
 repos <- getOption("repos")
@@ -11,13 +12,14 @@ libs <- .libPaths()
 
 if(!dir.exists(checkpoint_loc)) dir.create(checkpoint_loc)
 
+pkgcache::pkg_cache_delete_files()
 
 test_that("Creating checkpoint works",
 {
-    pkgcache::pkg_cache_delete_files()
-
     expect_true(dir.exists(checkpoint_loc))
     expect_false(dir.exists(file.path(checkpoint_loc, ".checkpoint")))
+
+    expect_error(create_checkpoint(snapshot, r_version="0.0.1"))
 
     inst <- create_checkpoint(snapshot, project_dir="../project", checkpoint_location=checkpoint_loc,
                               scan_now=FALSE)
@@ -28,8 +30,7 @@ test_that("Creating checkpoint works",
     expect_true(dir.exists(checkpoint_dir))
 
     inst <- create_checkpoint(snapshot, project_dir="../project", checkpoint_location=checkpoint_loc,
-                              scan_now=TRUE,
-                              use_now=FALSE)
+                              scan_now=TRUE)
     expect_is(inst, "pkg_installation_proposal")
     expect_true(length(dir(checkpoint_dir)) > 0)
 
@@ -38,9 +39,23 @@ test_that("Creating checkpoint works",
 })
 
 
+test_that("Creating checkpoint with different snapshot works",
+{
+    checkpoint_dir2 <- checkpoint_dir(snapshot2, checkpoint_loc, getRversion())
+
+    inst <- create_checkpoint(snapshot2, project_dir="../project", checkpoint_location=checkpoint_loc,
+                              scan_now=TRUE)
+    expect_is(inst, "pkg_installation_proposal")
+    expect_true(length(dir(checkpoint_dir2)) > 0)
+
+    pkg_srcs <- list_pkgsrc(checkpoint_dir2)
+    expect_true(all(grepl(snapshot2, pkg_srcs)))
+})
+
+
 test_that("Using checkpoint works",
 {
-    use_checkpoint("2020-01-01", checkpoint_location=checkpoint_loc)
+    use_checkpoint(snapshot, checkpoint_location=checkpoint_loc)
     expect_identical(getOption("repos")[1], c(CRAN=file.path(mran, "snapshot", snapshot)))
     expect_identical(.libPaths()[1], checkpoint_dir(snapshot, checkpoint_loc, getRversion()))
 })
