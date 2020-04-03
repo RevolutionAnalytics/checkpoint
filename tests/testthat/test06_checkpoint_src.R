524e4e2c83111df45c4b1252490ce557444a92c9
Hong Ooi <hongooi@microsoft.com>
2020-04-03 18:33:23 +1100

    more testing

--- tests/testthat/test06_checkpoint_src.R
+++ tests/testthat/test06_checkpoint_src.R
@@ -22,9 +22,10 @@ test_that("Checkpointing from source works",
 
     inst <- create_checkpoint(snapshot, checkpoint_location=checkpoint_loc, project_dir="../project_old",
                               config=list(platforms="source"))
-    expect_is(inst, "pkg_installation_proposal")
+    #expect_is(inst, "pkg_installation_proposal")
     checkpoint_dir <- checkpoint_dir(snapshot, checkpoint_loc, getRversion())
     expect_true(dir.exists(checkpoint_dir))
+    expect_identical(dir(checkpoint_dir), c("_cache", "R6"))
 })
 
 
