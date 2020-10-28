45d8b317d243fd24bd69138ca5d729527cffebbc
Hong Ooi <hongooi@microsoft.com>
2020-10-29 10:45:37 +1100

    redoc, change ownership back to hong

--- R/checkpoint.R
+++ R/checkpoint.R
@@ -22,7 +22,7 @@
 #' 
 #' @section Changing the default MRAN url:
 #' 
-#' By default, `checkpoint()` uses https to download packages. The default MRAN snapshot defaults to \url{https://mran.microsoft.com/snapshot} in R versions 3.2.0 and later, if https support is enabled.
+#' By default, `checkpoint()` uses https to download packages. The default MRAN snapshot defaults to \url{https://cran.microsoft.com/snapshot/} in R versions 3.2.0 and later, if https support is enabled.
 #' 
 #' You can modify the default URL. To change the URL, use `options(checkpoint.mranUrl = ...)`.
 #' 
